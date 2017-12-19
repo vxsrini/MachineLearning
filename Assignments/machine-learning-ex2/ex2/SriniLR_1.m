168f21f5323f56dff6d5d42489da87b85ba99cbe
vxsrini <vxsrini@gmail.com>
2017-12-19 17:33:20 -0500

    Working costFuntion

--- Assignments/machine-learning-ex2/ex2/SriniLR_1.m
+++ Assignments/machine-learning-ex2/ex2/SriniLR_1.m
@@ -15,36 +15,8 @@ plot(x(neg, 1), x(neg, 2), 'o')
 X = x;
 X = [ones(rows(x), 1), x]
 
-
-if (1 == 2)
-	newTheta = oldTheta = zeros(columns(dat), 1)
-	z = h = j = 0;
-	diffVector = []
-	alpha = .01;
-	count = 1;
-	lambda = [0,0,0;0,1,0;0,0,1]
-	while(1 == 2)
-	finalTheta = ((inv(x'* x) - (1000 * lambda)) * x' * y)
-	do
-		count++;
-		oldTheta = newTheta
-		for i = [1:rows(x)]
-			z = oldTheta' * x(i, :)';
-			h = (1 / (1+exp(-1 * z)));
-			diffVector = [diffVector; (h - y(i))];
-			j = j + ((-1 * y(i) * log(h)) - ((1 - y(i)) * log(1-h)));
-			cost = j / rows(x);
-		endfor
-	
-		for j = [1:rows(oldTheta)]
-			newTheta(j) = oldTheta(j) - ((alpha / rows(x)) * (diffVector' * x(:, j)));
-		endfor
-		diffVector = [];
-	until(sum((newTheta - oldTheta) < .00001*ones(rows(oldTheta), 1)) == columns(dat))
-	endwhile
-endif
-
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 initial_theta = zeros(columns(X), 1)
-[optTheta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options)
+%[optTheta, cost] = fminunc(@(t)(costFunction_1(t, X, y)), initial_theta, options)
+[optTheta, cost] = fminunc(@(initial_theta)(costFunction_1(initial_theta, X, y)), initial_theta, options)
 
--- Assignments/machine-learning-ex2/ex2/costFunction_1.m
+++ Assignments/machine-learning-ex2/ex2/costFunction_1.m
@@ -1,15 +1,8 @@
 function [jVal, gradient] =  costFunction_1(theta1, X, y)
 
-	theta1 
-	size(X)
-	size(y)
-	h = (1 / (1 + exp(-1 * X * theta1)));  %%% 100X3 * 3X1 = 47 X 1
-	h = h'
-	jVal = (-1/rows(X)) * (y' * log(h) + (1-y)' * log(1 - h)) %%% 1X47 * 47X1
-	
+	h = (1 ./ (1 + exp(-1 * X * theta1)));  
+	jVal = (-1/rows(X)) * ((y' * log(h)) + ((1-y)' * log(1 - h)));
 	gradient = zeros(rows(X), 1);
-	for i = [1:columns(X)]
-		gradient(i)  = X(:, i)' * (h - y)
-	endfor
-	
+	gradient = (1/rows(X)) * (X' * (h - y));
+
 endfunction
